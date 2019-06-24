52a39fc1bb06d28c62afbb0d6c924cb28992e628
prayaglehana <34983723+prayaglehana@users.noreply.github.com>
2019-06-25 04:31:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ class App extends Component{
       return (<div><Main  dealContract={dealContract}  thisAccount={this.state.thisAccount} ca={this.state.ca}  /></div>)
     }
     else
-     return ( <p>Loading ... ( Please Install Metamask extension First) </p> )
+     return ( <div><a href="https://www.w3schools.com">Please install metamask first !</a> </div>)
     
     
    
